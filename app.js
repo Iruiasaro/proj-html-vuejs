66ac24f2f472beed45dd8b2a2bd68497663f456d
Iruiasaro <brunettirosario@gmail.com>
2021-05-25 11:44:56 +0200

    inserimento footer e newsletter

--- app.js
+++ app.js
@@ -1,8 +1,20 @@
-var app = new Vue(
-    {
-        el: '#app',
-        data: {
-            linkNav: ['Home','About','Services','Blog','Contact','Portfolio'],
-        }
+ var app = new Vue(
+     {
+         el: '#app',
+         data: {
+             linkNav: ['Home','About','Services','Blog','Contact','Portfolio'],
+
+         footQuick: ['Services','Contact','Blog'],
+
+         footResources: ['Art Design','Computer','IT News','Networking','Web Security'],
+
+         footContactUs: {
+             Address: 'Patricia C.Amedee 4401 Waldeck Street Grapevine Nashville, Tx 76051',
+             Phone: '+99 (0) 101 0000 888',
+             Email: 'info@yourdomain.com'
+         },
+     }
     }
-)
+ )
+
+  
